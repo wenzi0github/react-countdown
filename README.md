46d4174043065b0c68c430c014d6d435e2b564f9
skeetershi(师少兵) <skeetershi@tencent.com>
2020-03-04 17:57:15 +0800

    feat(init): 初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # react-countdown
-react的倒计时组件
+
+# react 的倒计时组件
+
+这是基于抢金达人活动中使用的倒计时总结出来的。
+
+简单的使用：
+
+```jsx
+<CountDown
+    endTime={Date.now() + 4 * 1000}
+    format={progress => 'wenzi ' + progress}
+    diff={10}
+    onStep={step => console.log(step)}
+    onEnd={() => console.log('end')}
+/>
+```
+
+### 参数的说明：
+
+| 参数    | 定义               | 说明                                                 |
+| ------- | ------------------ | ---------------------------------------------------- |
+| total   | number             | 倒计时的时间                                         |
+| endTime | number             | 结束的时间点，与 total 二选一，且优先级更高          |
+| format  | string \| function | 要展示的时间格式，可以是字符串或者函数，下面相信说明 |
+| diff    | number             | 频率                                                 |
+| onStart | function           | 倒计时开始时的回调                                   |
+| onStep  | function(step)     | 每次更新时执行的回调                                 |
+| onEnd   | function           | 结束时的回调                                         |
+
+### format 的介绍
+
+该参数表示要展示的时间格式，可以是字符串或者函数。
+
+#### format 字符串
+
+完整的格式是：`dd hh:mm:ss.iii`。
+
+-   d: 天数
+-   h: 小时
+-   m: 分
+-   s: 秒
+-   i: 毫秒
+
+#### format 函数
+
+完整的格式：
+
+```javascript
+function(progress) {
+    return progress;
+}
+```
+
+接收的参数为距离结束时的毫秒级时间戳，返回的类型为 string 类型，即要展示的数据。
---
+++ src/index.tsx
@@ -0,0 +1,140 @@
+import { Component } from 'react';
+
+interface CountDownProps {
+    total?: number;
+    endTime?: number;
+    format: string | ((progress: number) => string); // 展示的格式
+    diff?: number; // 循环的时间
+    onStart?: () => void;
+    onStep?: (step: number) => void;
+    onEnd?: () => void;
+}
+
+interface StateProps {
+    endTime: number; // 结束时间
+    progress: number; // 当前时间
+    status: 'pending' | 'running' | 'suspend' | 'ended',
+}
+
+export default class CountDown extends Component<CountDownProps> {
+    timer: any = null;
+
+    state: StateProps = {
+        status: 'pending',
+        endTime: 0,
+        progress: 0
+    };
+
+    // 开始倒计时
+    start() {
+        this.execute('onStart');
+        this.setState({ status: 'running' });
+        const { diff, format } = this.props;
+
+        const running = () => {
+            const now = Date.now();
+            const progress = Math.max(this.state.endTime - now, 0);
+            let _progress = '';
+            if (typeof format==='string') {
+                _progress = this.formatTime(progress, format);
+            } else {
+                _progress = format.call(null, progress);
+            }
+
+            this.setState({ progress: _progress })
+            this.execute('onStep', _progress);
+
+            if (progress === 0) {
+                this.stop();
+            }
+            return progress;
+        }
+        if (diff && diff >= 17) {
+            this.timer = setInterval(() => {
+                running();
+            }, diff);
+        } else {
+            const requestAnimFrame =
+                window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                function (callback) {
+                    window.setTimeout(callback, 1000 / 60);
+                };
+
+            setTimeout(() => {
+                (function loop() {
+                    const progress = running();
+                    if (progress > 0) {
+                        requestAnimFrame(loop);
+                    }
+                })();
+            }, 0)
+        }
+    }
+
+    // 停止倒计时
+    stop() {
+        clearInterval(this.timer);
+        this.setState({ status: 'ended' });
+        this.execute('onEnd');
+    }
+
+    componentDidMount() {
+        const { endTime, total } = this.props;
+
+        if (!endTime && !total) {
+            // 至少需要一个参数
+            console.error(`endTime and total need least one`);
+        } else {
+            const now = Date.now();
+            let _endTime = 0;
+            if (!endTime) {
+                _endTime = now + (total || 0);
+            } else {
+                _endTime = endTime;
+            }
+            if (_endTime < now) {
+                // 若结束时间本来就小于当前的时间，直接结束
+                this.stop();
+            } else {
+                this.setState({ endTime: _endTime });
+                this.start();
+            }
+        }
+    }
+
+    formatTime(timestamp: number, format: string = 'dd hh:mm:ss.ii'): string {
+        const dateFormat: any = {
+            'd+': Math.floor(timestamp / 1000 / 60 / 60 / 24), // 天
+            'h+': Math.floor(timestamp / 1000 / 60 / 60 % 24), // 时
+            'm+': Math.floor(timestamp / 1000 / 60 % 60), // 分
+            's+': Math.floor((timestamp / 1000) % 60),  // 秒
+            'i+': timestamp % 1000 // 毫秒
+        };
+
+        for (let key in dateFormat) {
+            if (new RegExp('(' + key + ')').test(format)) {
+                format = format.replace(RegExp.$1, () => {
+                    const regLen = RegExp.$1.length;
+                    if (/i+/.test(RegExp.$1)) {
+                        return (dateFormat[key] + '000').substr(0, regLen);
+                    }
+                    return regLen === 1 ? dateFormat[key] : ('00' + dateFormat[key]).substr(('' + dateFormat[key]).length)
+                });
+            }
+        }
+        return format;
+    };
+
+    // 触发props中传过来的回调
+    execute(fn: string, ...params: string[] | number[]) {
+        const func = (this.props as any)[fn];
+        if (typeof func === 'function') {
+            func(...params);
+        }
+    }
+
+    render() {
+        return (<p>{this.state.progress}</p>);
+    }
+}
